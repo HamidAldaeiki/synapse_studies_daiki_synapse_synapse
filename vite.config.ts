99f8560bc9cbab242859861d809e34a93359ecde
HamidAldaeiki <71213775+HamidAldaeiki@users.noreply.github.com>
2025-12-10 19:46:38 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "www.synapsestudies.com",
+  base: "www.synapsestudies.space",
   plugins: [react()],
 })
