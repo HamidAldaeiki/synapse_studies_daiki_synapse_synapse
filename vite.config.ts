afe8e98d03f0819f3265109e32a576f6a6e2b48d
Hamid Aldaiki <hamidaldaeiki@gmail.com>
2025-12-10 18:35:14 +0200

    Fix base path for custom domain

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/synapse_studies_daiki_synapse_synapse",
+  base: "/",
   plugins: [react()],
 })
