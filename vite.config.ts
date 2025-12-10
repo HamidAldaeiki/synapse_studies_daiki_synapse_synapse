9892b4e82f514e945d760a6670084f92f2bb95df
HamidAldaeiki <71213775+HamidAldaeiki@users.noreply.github.com>
2025-12-10 12:19:50 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: './', // يجعل كل المسارات نسبية للعمل على GitHub Pages
+  base: "/synapse_studies_daiki_synapse_synapse", // يجعل كل المسارات نسبية للعمل على GitHub Pages
   plugins: [react()],
 })
