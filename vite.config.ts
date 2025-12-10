494871be01bb7f10bbe46d37178123203a3fe5d8
Hamid Aldaiki <hamidaldaeiki@gmail.com>
2025-12-10 12:55:57 +0200

    first commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,8 @@
-﻿import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+﻿import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  base: "/Synapse_Studies/",
+  base: "/synapse_studies_daiki_synapse_synapse", // 
   plugins: [react()],
-  server: {
-    port: 5173
-  },
-  resolve: {
-    alias: [
-      { find: "@", replacement: "/src" }
-    ]
-  }
-});
+})
