8c74055f6d7f8b64cc65b31c58a214df65ac3f93
HamidAldaeiki <71213775+HamidAldaeiki@users.noreply.github.com>
2025-12-10 19:24:54 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-<<<<<<< HEAD
   base: "/",
-=======
-  base: "/",
->>>>>>> 4c344df7de75fc3a37e8f4f2257743db257b01da
   plugins: [react()],
 })
